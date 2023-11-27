52c0de52b6482de484a5a8376bfe8de26c076001
eycho <chw3872@gmail.com>
2023-11-27 19:12:16 +0900

    mysql migration

--- BaseballComm/BaseballComm/settings.py
+++ BaseballComm/BaseballComm/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = local_settings.SECRET_KEY
+DATABASES = local_settings.DATABASES
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&^cw)=qv0q+6e8%u#i(wq+si*_$dmqyv&h9iehje)#-i)2nm+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -69,16 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BaseballComm.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
