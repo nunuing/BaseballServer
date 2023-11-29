686c0904a61cf22e09af188492f6563fbe866eab
eycho <chw3872@gmail.com>
2023-11-29 20:31:21 +0900

    model setting

--- BaseballComm/BaseballComm/settings.py
+++ BaseballComm/BaseballComm/settings.py
@@ -61,6 +61,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_WHITELIST  = (
+    'http://127.0.0.1:8000', 'http://localhost:3000'
+    )
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'BaseballComm.urls'
 
 TEMPLATES = [
